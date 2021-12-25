a4a74b4f053613559cd1d0fa86a049e64f7a0db2
borfirbora <borafirlangec@gmail.com>
2021-12-25 23:51:24 +0300

    asdfsadf

--- bot.js
+++ bot.js
@@ -93,6 +93,6 @@ const web = http.createServer((req, res) => {
   res.end("Sunucu başarıyla çalışıyor...")
 })
 
-web.listen(process.env.port | 3000, () => {
+web.listen(process.env.PORT|3000, () => {
   console.log("Sunucu çalışıyor...")
 })
